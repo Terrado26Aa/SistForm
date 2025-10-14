42817e38bf9307bb1f49c20624c3ba87bfb03f8e
Terrado26Aa <cesarterrado26@gmail.com>
2025-10-14 00:15:14 -0500

    [modelos backend] Agregado: AuthLogin/Models/RegisterRequestDto.cs | Modificado: AuthLogin/Models/User.cs

---
+++ AuthLogin/Models/RegisterRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthLogin.Models
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
