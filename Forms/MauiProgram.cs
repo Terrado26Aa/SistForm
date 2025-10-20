6dd9100452efa19884028a8d0e01f94a47f5cd1a
Terrado26Aa <cesarterrado26@gmail.com>
2025-10-19 22:37:27 -0500

    [configuración app] Modificado: Forms/MauiProgram.cs

--- Forms/MauiProgram.cs
+++ Forms/MauiProgram.cs
@@ -12,12 +12,12 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+			.UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-			.UseMauiCommunityToolkit();
+			});
 		
 
 #if DEBUG
