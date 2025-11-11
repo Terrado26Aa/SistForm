2c5adbc6c04488dd038633c3aa4c2e2d8b3c8440
Terrado26Aa <cesarterrado26@gmail.com>
2025-11-10 22:48:51 -0500

    [controladores] Modificado: AuthLogin/Controllers/AuthController.cs, AuthLogin/Controllers/WeatherForecastController.cs

--- AuthLogin/Controllers/AuthController.cs
+++ AuthLogin/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AuthLogin.Controllers
 {
-    [Route ("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
