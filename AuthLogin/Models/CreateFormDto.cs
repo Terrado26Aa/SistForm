0e1a28127f0657f44bde1b2205ad891f16fddbde
Terrado26Aa <cesarterrado26@gmail.com>
2025-12-06 23:48:21 -0500

    [modelos backend] Agregado: AuthLogin/Models/CForm.cs, AuthLogin/Models/CreateFormDto.cs

---
+++ AuthLogin/Models/CreateFormDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthLogin.Models
+{
+    public class CreateFormDto
+    {
+        public int IdUser { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
