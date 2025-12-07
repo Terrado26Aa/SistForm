be8759947945c6722350b0899800cc8ef1877e8b
Terrado26Aa <cesarterrado26@gmail.com>
2025-12-06 23:48:21 -0500

    [controladores] Agregado: AuthLogin/Controllers/FormsController.cs

---
+++ AuthLogin/Controllers/FormsController.cs
@@ -0,0 +1,37 @@
+﻿using AuthLogin.Data;
+using AuthLogin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FormsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateForm([FromBody] CreateFormDto createFormDto)
+        {
+            if (createFormDto == null) return BadRequest("Datos del formulario son requeridos");
+
+            var newForm = new CForm
+            {
+                IdUser = createFormDto.IdUser,
+                Title = createFormDto.Title,
+                Description = createFormDto.Description,
+                CreationDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            };
+
+            _context.CForms.Add(newForm);
+            await _context.SaveChangesAsync();
+            return Ok(new { Message = "Formulario creado exitosamente", FormId = newForm.IdForm });
+        }
+    }
+}
