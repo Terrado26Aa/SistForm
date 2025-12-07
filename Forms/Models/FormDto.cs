a455e05677ef7d0cdcb059a126976835fee34286
Terrado26Aa <cesarterrado26@gmail.com>
2025-12-06 23:48:21 -0500

    [modelos frontend] Agregado: Forms/Models/FormDto.cs

---
+++ Forms/Models/FormDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms.Models
+{
+    public class FormDto
+    {
+        public int IdUser { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+
+        public List<FormElementDto> Elements { get; set; } = new List<FormElementDto>();
+    }
+
+    public class FormElementDto
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+    }
+}
