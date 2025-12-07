79ec3dfee3d377daa1a3a3792c2075bfd91fbef8
Terrado26Aa <cesarterrado26@gmail.com>
2025-12-06 23:48:21 -0500

    [controladores] Modificado: AuthLogin/Controllers/AuthController.cs

--- AuthLogin/Controllers/AuthController.cs
+++ AuthLogin/Controllers/AuthController.cs
@@ -3,6 +3,10 @@ using AuthLogin.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AuthLogin.Controllers
 {
@@ -11,15 +15,18 @@ namespace AuthLogin.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ApplicationDbContext _context; //Inyecta tu DbContext
+        private readonly IConfiguration _configuration; //Inyecta la Configuracion
 
-        public AuthController(ApplicationDbContext context)
+        public AuthController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
         {
+            // validar que no sea nulo o vacio
             if (loginRequest == null || string.IsNullOrWhiteSpace(loginRequest.UserName)
                 || string.IsNullOrWhiteSpace(loginRequest.Password))
             {
@@ -32,7 +39,7 @@ namespace AuthLogin.Controllers
             if (user == null)
             {
                 // Usuario no encontrado
-                return Unauthorized("Usuario no encontrado");
+                return Unauthorized("Usuario o contraseña incorrecto");
             }
 
             // Verifica la contraseña usando BCrypt
@@ -40,10 +47,55 @@ namespace AuthLogin.Controllers
 
             if (!isPasswordValid)
             {
-                return Unauthorized("Contraseña incorrecta");
+                return Unauthorized("Usuario o contraseña incorrecta");
             }
 
-            return Ok(new { Message = $"Bienvenido {user.UserName}!", UserId = user.Id });
+            var tokenString = GenerateTokenJWT(user);
+            return Ok(new { Message = $"Bienvenido {user.UserName}!", UserId = user.Id, Token = tokenString });
+        }
+
+        // Generar el token JWT
+        private string GenerateTokenJWT(User user)
+        {
+            // Obtener la clave secreta desde la configuración appsettings.json
+            var secretKey = _configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                throw new InvalidOperationException("La clave secreta JWT no está configurada en appsettings.json");
+            }
+            var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+
+            // Crea los claims(informacion que ira dentro del token)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            // Crea las credenciales de firma
+            var singingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(keyBytes),
+                SecurityAlgorithms.HmacSha256Signature
+                );
+
+            // Crea el descriptor del token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = singingCredentials,
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            // Crea el token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Devuelve el token en formato string
+            return tokenHandler.WriteToken(token);
         }
 
         [HttpPost("register")]
