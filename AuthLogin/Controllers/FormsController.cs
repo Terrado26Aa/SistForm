16c06201f2c0a71d34dc303f0467cd50acce59f6
Terrado26Aa <cesarterrado26@gmail.com>
2026-02-09 00:38:16 -0500

    [controladores] Modificado: AuthLogin/Controllers/FormsController.cs

--- AuthLogin/Controllers/FormsController.cs
+++ AuthLogin/Controllers/FormsController.cs
@@ -20,15 +20,30 @@ namespace AuthLogin.Controllers
         {
             if (createFormDto == null) return BadRequest("Datos del formulario son requeridos");
 
+            //Crea el formulario padre
             var newForm = new CForm
             {
                 IdUser = createFormDto.IdUser,
                 Title = createFormDto.Title,
                 Description = createFormDto.Description,
                 CreationDate = DateTime.Now,
-                LastModifiedDate = DateTime.Now
+                LastModifiedDate = DateTime.Now,
+                Elements = new List<CFormElement>() //Inicializa la lista
             };
 
+            //Mapea los elementos del DTO a la entidad
+            if (createFormDto.Elements != null) {
+                foreach (var item in createFormDto.Elements)
+                {
+                    newForm.Elements.Add(new CFormElement
+                    {
+                        Title = item.Title,
+                        Type = item.Type
+                    });
+                }
+            }
+
+            //Guarda todo de una vez
             _context.CForms.Add(newForm);
             await _context.SaveChangesAsync();
             return Ok(new { Message = "Formulario creado exitosamente", FormId = newForm.IdForm });
