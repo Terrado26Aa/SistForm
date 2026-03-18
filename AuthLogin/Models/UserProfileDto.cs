a4d6d2cd9f57bc70cff998560562ed4b6e4d393e
Terrado26Aa <cesarterrado26@gmail.com>
2026-03-18 01:40:53 -0500

    [modelos backend] Agregado: AuthLogin/Models/UserProfileDto.cs

---
+++ AuthLogin/Models/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthLogin.Models
+{
+    public class UserProfileDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
