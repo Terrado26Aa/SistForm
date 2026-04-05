37b8699b4dea58e57bbfb609c5cbc45881c89c92
Terrado26Aa <cesarterrado26@gmail.com>
2026-04-05 00:44:59 -0500

    [modelos frontend] Modificado: Forms/Models/UserProfileDto.cs

--- Forms/Models/UserProfileDto.cs
+++ Forms/Models/UserProfileDto.cs
@@ -11,6 +11,6 @@ namespace Forms.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? NewPassword { get; set; }
     }
 }
