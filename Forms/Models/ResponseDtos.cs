c650b17d23693070a1f363965971a3b0e03b9dff
Terrado26Aa <cesarterrado26@gmail.com>
2026-05-02 23:10:29 -0500

    [modelos frontend] Modificado: Forms/Models/ResponseDtos.cs | Eliminado: Forms/Models/ToolbarItem.cs

--- Forms/Models/ResponseDtos.cs
+++ Forms/Models/ResponseDtos.cs
@@ -19,13 +19,25 @@ namespace Forms.Models
         //Propiedades para mostrar en la lista de respuestas pendientes
         public string FormTitle { get; set; }
         public DateTime SaveAt { get; set; } = DateTime.Now; //Guarda la fecha y hora.
+
+        public double? LatitudeA { get; set; }
+        public double? LongitudeA { get; set; }
+        public double? LatitudeB { get; set; }
+        public double? LongitudeB { get; set; }
+        public List<TrackPoint>? TrackPoints { get; set; }
+    }
+
+    public class TrackPoint
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
     }
 
     public class ResponseDetailDto
     {
         public int FormElementId { get; set; }
         //no esta siendo usada, pero se puede usar para mostrar el titulo de la pregunta en la lista de respuestas pendientes
-        public string QuestionTitle { get; set; }
+        public string Question { get; set; }
         public string Answer { get; set; }
     }
 }
