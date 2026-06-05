5e6bb552dd38983449425b421412aa741297009d
Terrado26Aa <cesarterrado26@gmail.com>
2026-06-05 01:17:59 -0500

    [datos] Modificado: AuthLogin/Data/ApplicationDbContext.cs

--- AuthLogin/Data/ApplicationDbContext.cs
+++ AuthLogin/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using AuthLogin.Models;
+using AuthLogin.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
@@ -21,16 +21,39 @@ namespace AuthLogin.Data
 
         public DbSet<CFormResponse> FormResponses { get; set; }
         public DbSet<CFormResponseDetail> FormResponseDetails { get; set; }
+        public DbSet<FormAssignment> FormAssignments { get; set; }
 
 
         //Configuración del modelo de datos
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            
+            // Configuración de User
             modelBuilder.Entity<User>(entity =>
             {
-                entity.ToTable("Users"); //asegura el nombre de la tabla
+                entity.ToTable("users"); // Nombre real de la tabla en MySQL (minúsculas)
                 entity.HasIndex(u => u.UserName).IsUnique(); //Indice único
+            });
+
+            // Configuración de CForm
+            modelBuilder.Entity<CForm>(entity =>
+            {
+                entity.ToTable("createforms");
+                entity.HasKey(e => e.IdForm);
+                entity.HasMany(e => e.Elements)
+                      .WithOne(e => e.Form)
+                      .HasForeignKey(e => e.FormId)
+                      .HasConstraintName("form_elements_ibfk_1")
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Configuración de CFormElement
+            modelBuilder.Entity<CFormElement>(entity =>
+            {
+                entity.ToTable("form_elements");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
             });
         }
     }
